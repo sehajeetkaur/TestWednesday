7f3ba3086b6f3aa1b0429529727d6429451f96ac
sehajeetkaur <kasehaje@sheridancollege.ca>
2024-05-15 16:15:49 -0400

    changed remote file

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * 
  * @author dancye May 7, 2018
  * @modified by Sehaj May 15 , 2024
+ making a change on github file
  */
 public class Student 
 {
